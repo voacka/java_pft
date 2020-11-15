a16ada2f40d901699eb272bcd9702e96688ff345
voacka <vdovin91@yandex.ru>
2020-11-16 01:28:08 +0300

    Переменные и значения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Sanya, let's go sleep!");
+        String something = "World! ";
+        System.out.println("Hello, " + something);
 
+        int l = 10;
+        int s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
 	}
 }
