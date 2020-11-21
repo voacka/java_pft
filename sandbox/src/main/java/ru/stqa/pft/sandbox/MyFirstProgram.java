ccbb98f94a4938cc84b0af8c65549fc226962fae
voacka <vdovin91@yandex.ru>
2020-11-21 15:26:10 +0300

    Hello World + Square, Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,9 +3,9 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-//        hello("world");
-//        hello("user");
-//        hello("Victor");
+        hello("world");
+        hello("user");
+        hello("Victor");
 
         Square s = new Square(10);
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
@@ -14,9 +14,7 @@ public class MyFirstProgram {
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
-//    public static void hello(String somebody) {
-//        System.out.println("Hello, " + somebody + "!");
-//    }
-
-
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
 }
