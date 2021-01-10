a76eeb30af807370bba25b4ae95146ee43acce3c
voacka <vdovin91@yandex.ru>
2021-01-10 23:40:55 +0300

    LFC

--- addressbook-web-tests/src/test/java/ru/vdovin/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/vdovin/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void contactCreationTest() {
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactForm(new ContactData("Victor", "Vladimirovich", "Vdovin", "Voacka", "Afina LLC", "Moscow", "Zhuk", "+7-926-777-77-77", "+7-495-777-77-77", "vdovin@afina-pet.ru", "vdovin91@yandex.ru", "vdovin91@gmail.ru", "https://vk.com/id11120154"));
+        app.getContactHelper().fillContactForm(new ContactData("Victor", "Vladimirovich", "Vdovin", "Voacka", "LFC", "Moscow", "Zhuk", "+7-926-777-77-77", "+7-495-777-77-77", "vdovin@afina-pet.ru", "vdovin91@yandex.ru", "vdovin91@gmail.ru", "https://vk.com/id11120154"));
         app.getContactHelper().returnToHomePage();
     }
 
